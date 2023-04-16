a15b638e4222cdaf518673fb1769be99389575e2
Muhammad Mubashir Munir Khan <100374421+mubashir05-beep@users.noreply.github.com>
2023-04-16 03:18:22 -0700

    Update Work.jsx

--- react_todo/src/components/To-Do App/Work.jsx
+++ react_todo/src/components/To-Do App/Work.jsx
@@ -1,68 +1,100 @@
 import React, { useContext, useState } from "react";
-import { FiEdit3} from "react-icons/fi";
+import { FiEdit3 } from "react-icons/fi";
 import { MdDeleteForever } from "react-icons/md";
+import { GrFormClose } from "react-icons/gr";
 import "./todo.css";
 
 const Work = ({ id, title, description, setTodo }) => {
-  const [editTitle, setEditTitle] = useState('');
-  const [editDesc, setEditDesc] = useState('');
-  const [toggleModal,setToggleModal]=useState(false);
+  const [editTitle, setEditTitle] = useState("");
+  const [editDesc, setEditDesc] = useState("");
+  const [toggleModal, setToggleModal] = useState(false);
   function edit_Title(e) {
     setEditTitle(e.target.value);
   }
-  
+
   function edit_Desc(e) {
     setEditDesc(e.target.value);
   }
-  
+
   function handleEdit(e) {
     e.preventDefault();
     if (editTitle === "") {
       return;
     } else {
       setTodo((prevTodo) => {
-        return prevTodo.map(todo => {
+        return prevTodo.map((todo) => {
           if (todo.id === id) {
             return { ...todo, title: editTitle, description: editDesc };
           }
           return todo;
         });
       });
-      setEditTitle('');
-      setEditDesc('');
+      setEditTitle("");
+      setEditDesc("");
     }
   }
   function handleDelete(e) {
     e.preventDefault();
     setTodo((prevTodo) => {
-      return prevTodo.filter(item => item.id !== id);
+      return prevTodo.filter((item) => item.id !== id);
     });
-    setEditTitle('');
-    setEditDesc('');
+    setEditTitle("");
+    setEditDesc("");
   }
-  
-function handleModal(){
-  return setToggleModal(!toggleModal);
-}
+
+  function handleModal() {
+    return setToggleModal(!toggleModal);
+  }
 
   return (
     <div className="container">
       <div className="workTitleContainer">
         <div className="title">{title}</div>
         <div className="icons">
-          <FiEdit3 id="editIcn"  onClick={
-            handleModal
-          }/>
-          <MdDeleteForever id="editIcn" onClick={handleDelete}/>
+          <FiEdit3 id="editIcn" onClick={handleModal} />
+          <MdDeleteForever id="editIcn" onClick={handleDelete} />
         </div>
       </div>
       <div className="descModel">{description}</div>
-      {toggleModal&&( <form onSubmit={handleEdit}>
-        <input type="text" name="editName" value={editTitle} onChange={edit_Title} />
-        <input type="text" name="editDesc" value={editDesc} onChange={edit_Desc} />
-        <button type="submit">submit</button>
-      </form>)}
-     
+      <div className="formEditContainer">
+      
+          {toggleModal && (
+              <div className="formEdit">
+            <form onSubmit={handleEdit}>
+              <div className="inputText">
+                <div className="inputEditText">
+                  Change Title
+                </div>
+              <input
+                type="text"
+                name="editName"
+                value={editTitle}
+                onChange={edit_Title}
+              />
+              </div>
+             <div className="inputDesc">
+             <div className="inputEditDesc">
+                  Change Description
+                </div>
+             <input
+                type="text"
+                name="editDesc"
+                value={editDesc}
+                onChange={edit_Desc}
+              />
+             </div>
+              
+              <button type="submit">submit</button>
+            </form>
+            <div className="crossIcon">
+            <GrFormClose/>
+            </div>
+           
+            </div>
+          )}
+        </div>
+      
+    
     </div>
   );
 };
